7034610238f6e090166b52a024104b637a49af1c
haizhao <haizhao@efficode.com>
2015-03-13 09:55:49 +0800

    Task J5: Playtime: (2) Display different prices in different locales

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,17 @@ module ApplicationHelper
         end
         content_tag("div", attributes, &block)
     end
+
+    def price_localize(price)
+      # should fetch from db or some other sources.
+      usd2cny = 6.2612
+      usd2eur = 0.9423
+
+      if I18n.locale.to_s == 'zh'
+        price *= usd2cny
+      elsif I18n.locale.to_s == 'es'
+        price *= usd2eur
+      end
+        price
+    end
 end
