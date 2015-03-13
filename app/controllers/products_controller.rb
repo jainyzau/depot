51e97cfb6e1679a644f7fda4b7ff8e904790c298
haizhao <haizhao@efficode.com>
2015-03-13 09:38:53 +0800

    Task J5: Playtime: (1) show different products in different locales

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to @product, notice: "Product was successfully updated." }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -80,6 +80,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price)
+      params.require(:product).permit(:title, :description, :image_url, :price, :locale)
     end
 end
